dde3b4965fdfc803c370ad0cd71d5d618e46c6c2
h4rithd <info@h4rithd.com>
2026-02-03 12:56:04 +0800

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="SecHead", 
-    version="1.0.0",
+    version="1.1.0",
     
     author="Harith Dilshan",
     author_email="contact@h4rithd.com",
